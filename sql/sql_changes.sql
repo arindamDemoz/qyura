5be5cc35d09b13c5a8bdfde68fb8d415ee02c587
mehpal <mehpal@mobileappz.in>
2016-01-29 12:05:05 +0530

    mehpal sql changes

--- sql/sql_changes.sql
+++ sql/sql_changes.sql
@@ -21,3 +21,20 @@ CREATE TABLE `qyura_hospitalServices` (
   `status` TINYINT(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`hospitalServices_id`)
 ) ENGINE=INNODB DEFAULT CHARSET=latin1
+
+DROP TABLE `vsure_qyura`.`qyura_reviews-post`; 
+
+CREATE TABLE `qyura_reviewsPost` (
+  `reviewsPost_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `reviewsPost_reviewsId` INT(11) UNSIGNED NOT NULL,
+  `reviewsPost_usersId` INT(11) UNSIGNED NOT NULL COMMENT 'Who posts comment for this review',
+  `reviewsPost_details` VARCHAR(555) NOT NULL,
+  `reviewsPost_deleted` TINYINT(1) DEFAULT '0' COMMENT '1 fro deleted  1 for not deleted',
+  `creationTime` BIGINT(20) NOT NULL,
+  `modifyTime` BIGINT(20) NOT NULL,
+  `status` TINYINT(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`reviewsPost_id`),
+  KEY `reviewsPost_reviewsId` (`reviewsPost_reviewsId`),
+  CONSTRAINT `qyura_reviewsPost_ibfk_1` FOREIGN KEY (`reviewsPost_reviewsId`) REFERENCES `qyura_reviews` (`reviews_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=latin1
+
