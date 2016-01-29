fa2f1f5c1b380e7c8fcab4f125495e6e28b32062
mehpal <mehpal@mobileappz.in>
2016-01-29 16:56:04 +0530

    mehpal sql changes

--- sql/sql_changes.sql
+++ sql/sql_changes.sql
@@ -38,3 +38,42 @@ CREATE TABLE `qyura_reviewsPost` (
   CONSTRAINT `qyura_reviewsPost_ibfk_1` FOREIGN KEY (`reviewsPost_reviewsId`) REFERENCES `qyura_reviews` (`reviews_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=INNODB DEFAULT CHARSET=latin1
 
+CREATE TABLE `qyura_hospitalDiagnosticsCat` (
+  `hospitalDiagnosticsCat_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `hospitalDiagnosticsCat_hosUserId` INT(11) UNSIGNED NOT NULL COMMENT 'Hospital Pk Id',
+  `hospitalDiagnosticsCat_diagnosticsCatId` INT(11) UNSIGNED NOT NULL COMMENT 'Services_id Pk id',
+  `hospitalDiagnosticsCat_deleted` TINYINT(1) DEFAULT NULL,
+  `creationTime` INT(10) DEFAULT NULL,
+  `modifyTime` INT(10) DEFAULT NULL,
+  `status` TINYINT(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`hospitalDiagnosticsCat_id`)
+) ENGINE=INNODB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1
+
+CREATE TABLE `qyura_diagnosticServices` (
+	`diagnosticServices_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`diagnosticServices_dignosticUserId` INT(11) UNSIGNED NOT NULL COMMENT 'diagnostic Pk Id',
+	`diagnosticServices_servicesId` INT(11) UNSIGNED NOT NULL COMMENT 'Services_id Pk id',
+	`diagnosticServices_deleted` TINYINT(1) NULL DEFAULT NULL,
+	`creationTime` INT(10) NULL DEFAULT NULL,
+	`modifyTime` INT(10) NULL DEFAULT NULL,
+	`status` TINYINT(1) NOT NULL DEFAULT '1',
+	PRIMARY KEY (`diagnosticServices_id`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+
+
+CREATE TABLE `qyura_diagnosticSpecialities` (
+	`diagnosticSpecialities_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`diagnosticSpecialities_specialitiesId` INT(11) UNSIGNED NOT NULL COMMENT 'Specialities Pk ID',
+	`diagnosticSpecialities_diagnosticUsersId` INT(11) UNSIGNED NOT NULL COMMENT 'diagnostic Pk Id',
+	`diagnosticSpecialities_deleted` TINYINT(1) NULL DEFAULT NULL,
+	`creationTime` INT(10) NULL DEFAULT NULL,
+	`modifyTime` INT(10) NULL DEFAULT NULL,
+	PRIMARY KEY (`diagnosticSpecialities_id`)
+	
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+
+
