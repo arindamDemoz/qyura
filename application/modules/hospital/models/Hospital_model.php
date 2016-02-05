b6cfd1f626f294e668283c096d0f5f05524c6f52
arindam <aris.mobileappz@google.com>
2016-02-05 21:24:56 +0530

    file uploaded by arindam for add hospital and view hospital details

--- application/modules/hospital/models/Hospital_model.php
+++ application/modules/hospital/models/Hospital_model.php
@@ -42,12 +42,60 @@ class Hospital_model extends CI_Model {
 
         return  $insert_id;
     }
-    function insertHospita($insertData){
+    function insertHospital($insertData){
         
         $this->db->insert('qyura_hospital', $insertData); 
       
         $insert_id = $this->db->insert_id();
 
         return  $insert_id;
+    }
+    
+    function insertBloodbank($insertData){
+        
+        $this->db->insert('qyura_bloodBank', $insertData); 
+      
+        $insert_id = $this->db->insert_id();
+
+        return  $insert_id;
+    }
+    
+    function insertPharmacy($insertData){
+        $this->db->insert('qyura_pharmacy', $insertData); 
+      
+        $insert_id = $this->db->insert_id();
+
+        return  $insert_id;
+    }
+    
+    function insertAmbulance($insertData){
+        $this->db->insert('qyura_ambulance', $insertData); 
+      
+        $insert_id = $this->db->insert_id();
+
+        return  $insert_id;
+        
+    }
+    
+    function insertHospitalServiceName($insertData){
+        $this->db->insert('qyura_hospitalServices', $insertData); 
+      
+        $insert_id = $this->db->insert_id();
+
+        return  $insert_id;
+        
+    }
+    
+    function fetchHotelData(){
+       $this->db->select('Hos.hospital_id,Hos.hospital_usersId,Hos.hospital_name,Hos.hospital_phn,Hos.hospital_address,City.city_name,Hos.hospital_img');
+     $this->db->from('qyura_hospital AS Hos');
+     $this->db->join('qyura_city AS City','City.city_id = Hos.hospital_cityId','left');
+    $this->db->where(array('Hos.hospital_deleted'=> 0));
+       $this->db->order_by("Hos.creationTime", "desc"); 
+      $data= $this->db->get(); 
+     return $data->result();
+      //echo $this->db->last_query(); exit;
+      //echo "<pre>";print_r($data);echo "</pre>";
+      //exit;
     }
 }
