d9297e323a53950394b0810df2c97b83c0bf91fd
pawansen <pawansen.mobileappz@gmail.com>
2016-02-24 12:57:51 +0530

    separat header footer create method in my loader pawan

---
+++ application/views/superAdmin_page/super_admin_footer.php
@@ -0,0 +1,15 @@
+<footer class="footer text-right">
+      2015 © Qyura.
+</footer>
+</div>
+<!-- End Right content here -->
+    </div>
+    <!-- END wrapper -->
+<script>
+var resizefunc = [];
+</script>
+     <script src="<?php echo base_url();?>assets/js/jquery-1.8.2.min.js"> </script>
+     <script src="<?php echo base_url();?>assets/js/framework.js"></script>
+     <script type= 'text/javascript' src="<?php echo base_url(); ?>assets/js/jquery.dataTables.js"></script>
+</body>
+</html>
