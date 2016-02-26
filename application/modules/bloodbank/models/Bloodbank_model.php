b225b600c57063666d47dfc659017a9c996d6d58
arindam <aris.mobileappz@gmail.com>
2016-02-26 17:29:35 +0530

    data table search

--- application/modules/bloodbank/models/Bloodbank_model.php
+++ application/modules/bloodbank/models/Bloodbank_model.php
@@ -21,15 +21,17 @@ class Bloodbank_model extends CI_Model {
     }
     
     function fetchEmail($email,$usersId = NULL){
-        $this->db->select('users_email');
+       $this->db->select('users_email');
         $this->db->from('qyura_users');
+        $this->db->join('qyura_usersRoles','qyura_usersRoles.usersRoles_userId = qyura_users.users_id','left');
         if($usersId) {
-            $this->db->where('users_id !=',$usersId);
+            $this->db->where('qyura_users.users_id !=',$usersId);
         }
-         $this->db->where('users_email',$email); 
+        $this->db->where('qyura_usersRoles.usersRoles_roleId',2);
+         $this->db->where('qyura_users.users_email',$email); 
        $result = $this->db->get();
        //return $this->db->last_query();
-       //exit;
+       
         if($result->num_rows() > 0)
             return 1;
         else             
@@ -72,15 +74,16 @@ class Bloodbank_model extends CI_Model {
             
          $imgUrl = base_url().'assets/BloodBank/thumb/original/$1';    
          
-         $this->datatables->select('blood.bloodBank_id,blood.users_id,blood.bloodBank_name,blood.bloodBank_phn,blood.bloodBank_add,City.city_name,'
+     $this->datatables->select('blood.bloodBank_id,blood.users_id,blood.bloodBank_name,blood.bloodBank_phn,blood.bloodBank_add,City.city_name,'
                  . 'blood.bloodBank_photo,usr.users_email,usr.users_password ,blood.bloodBank_cntPrsn,blood.bloodBank_lat,blood.bloodBank_long,'
-                  . "CASE WHEN bloodBank_photo IS NULL THEN 'noImage.png' ELSE bloodBank_photo END AS bloodBank_photo ");
+                  . "(CASE WHEN bloodBank_photo IS NULL OR bloodBank_photo = '' THEN 'noImage.png' ELSE bloodBank_photo END )AS bloodBank_photo ");
          
      $this->datatables->from('qyura_bloodBank AS blood');
      $this->datatables->join('qyura_city AS City','City.city_id = blood.cityId','left');
      $this->datatables->join('qyura_users AS usr','usr.users_id = blood.users_id','left');
  
-          $search = $this->input->post('bloodBank_name');
+         $search = $this->input->post('bloodBank_name');
+   
         if($search){
             $this->db->or_like('blood.bloodBank_add',$search);
             $this->db->or_like('blood.bloodBank_name',$search);
@@ -97,17 +100,19 @@ class Bloodbank_model extends CI_Model {
       $this->datatables->order_by('bloodBank_id');
         
      if($condition)
-        $this->datatables->where(array('blood.bloodBank_id'=> $condition));   
+     {
+        $this->datatables->where(array('blood.bloodBank_id'=> $condition));  
+     }
         $this->datatables->where(array('blood.bloodBank_deleted'=> 0));
         
-       $this->datatables->add_column('bloodBank_photo', '<img class="img-responsive" height="80px;" width="80px;" src='.$imgUrl.'>', 'bloodBank_photo');
+       //$this->datatables->add_column('bloodBank_photo', '<img class="img-responsive" height="80px;" width="80px;" src='.$imgUrl.'>', 'bloodBank_photo');
        
               /*$this->datatables->add_column('open','08 AM-12 AM');
               $this->datatables->add_column('call','03 PM-08 PM');*/
        
        $this->datatables->add_column('view', '<a class="btn btn-warning waves-effect waves-light m-b-5 applist-btn" href="bloodbank/detailBloodBank/$1">View Detail</a>', 'bloodBank_id');
        
-                    $this->datatables->add_column('bloodBank_add', '$1 </br><a  href="view-map.html" class="btn btn-info btn-xs waves-effect waves-light" target="_blank">View Map</a>', 'bloodBank_add');
+       $this->datatables->add_column('bloodBank_add', '$1 </br><a  href="view-map.html" class="btn btn-info btn-xs waves-effect waves-light" target="_blank">View Map</a>', 'bloodBank_add');
        
         return $this->datatables->generate(); 
       //echo $this->datatables->last_query();
